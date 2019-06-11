c5fafeb6fc99256b4b5e461b7226d3a4624e17c4
Kam1Cadzee <39504960+Kam1Cadzee@users.noreply.github.com>
2019-06-11 12:42:58 +0300

    Updates

---
+++ precache-manifest.5a1d8d11933a2e61e39e364d761f87d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31c58623e6732b8032959145cd079f6",
+    "url": "/goit-react-hw-05-budget-planner-fork/index.html"
+  },
+  {
+    "revision": "75bc18bee4b1b8e08b89",
+    "url": "/goit-react-hw-05-budget-planner-fork/static/css/main.67e7994e.chunk.css"
+  },
+  {
+    "revision": "5422c90600c2b32ad619",
+    "url": "/goit-react-hw-05-budget-planner-fork/static/js/2.f7c9d375.chunk.js"
+  },
+  {
+    "revision": "75bc18bee4b1b8e08b89",
+    "url": "/goit-react-hw-05-budget-planner-fork/static/js/main.b3ed251a.chunk.js"
+  },
+  {
+    "revision": "d2a8409cdc7aa031e03d",
+    "url": "/goit-react-hw-05-budget-planner-fork/static/js/runtime~main.eeee4730.js"
+  }
+]);
